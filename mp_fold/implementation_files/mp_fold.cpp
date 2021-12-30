61b768e7d1c18fd0cb841efada560d3f361fcbea
arnoldt <arnoldt@ornl.gov>
2021-12-30 13:01:18 -0500

    Added the class Person1.
    Added the class Person1 and made a typelist from Person1.

--- mp_fold/implementation_files/mp_fold.cpp
+++ mp_fold/implementation_files/mp_fold.cpp
@@ -1,13 +1,15 @@
-/*
- * mp_fold.cpp
- *
- *  Created on: Dec 28, 2021
- *      Author: arnoldt
- */
+//
+// mp_fold.cpp
+//
+//  Created on: Dec 28, 2021
+//      Author: arnoldt
+//
 
 #include "mp_fold.hpp"
 
 int main (int argc, char** argv)
 {
+    using Person_type_list = boost::mp11::mp_list<MCD_MP_FOLD::Person1>;
+
     return 0;
 }
