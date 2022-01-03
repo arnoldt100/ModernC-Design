e5e9924c7a792280a5a4ddb76a7988787d3a1697
arnoldt <arnoldt@ornl.gov>
2022-01-03 13:22:43 -0500

    Initial tracking of files Name.hpp and Name.cpp.

--- mp_fold/header_files/Jane.hpp
+++ mp_fold/header_files/Jane.hpp
@@ -1,16 +1,11 @@
 #include <boost/mp11.hpp>
 #include <iostream>
 #include <boost/core/demangle.hpp> 
-#include "Person1.h"
-#include "Person2.h"
-#include "Person3.h"
 #include "GenerateScatteredHierarchy.hpp"
 
 namespace MCD_MP_FOLD {
 
-using Person1_type_list = boost::mp11::mp_list<MCD_MP_FOLD::Person1>;
-
-class Jane : public boost::mp11::mp_front<Person1_type_list> 
+class Jane 
 {
     public: 
         Jane (); 
