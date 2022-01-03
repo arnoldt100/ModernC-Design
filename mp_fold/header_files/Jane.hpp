3039262f66cf55efca2f27dc94f39ab1b5517499
arnoldt <arnoldt@ornl.gov>
2022-01-03 14:53:28 -0500

    Addeing new files.

--- mp_fold/header_files/Jane.hpp
+++ mp_fold/header_files/Jane.hpp
@@ -10,7 +10,7 @@ class Jane
     public: 
         Jane (); 
     
-        ~Jane(); 
+        virtual ~Jane(); 
 
 };
 
--- mp_fold/implementation_files/mp_fold.cpp
+++ mp_fold/implementation_files/mp_fold.cpp
@@ -6,6 +6,10 @@
 //
 
 #include "mp_fold.hpp"
+#include "Name.hpp"
+#include "Person1.h"
+#include "Person2.h"
+#include "Person3.h"
 #include <iostream>
 #include <typeinfo>
 #include "GenerateScatteredHierarchy.hpp"
@@ -14,6 +18,10 @@ using Person_type_list = boost::mp11::mp_list<MCD_MP_FOLD::Person1,
                                               MCD_MP_FOLD::Person2,
                                               MCD_MP_FOLD::Person3>;
 
+using PersonWidget = MCD_MP_FOLD::GenerateScatteredHierarchy<MCD_MP_FOLD::Person1,
+                                                             MCD_MP_FOLD::Person2,
+                                                             MCD_MP_FOLD::Person3,
+                                                             MCD_MP_FOLD::Name>;
 
 int main (int argc, char** argv)
 {
