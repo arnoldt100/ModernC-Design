fd31a8fd26d944b0244d95f919555529e54adb69
arnoldt <arnoldt@ornl.gov>
2022-02-23 17:22:50 -0500

    Housekeeping.

---
+++ sources/abstract_factory_project/header_files/main.h
@@ -0,0 +1,6 @@
+#ifndef  main_INC
+#define  main_INC
+
+#include <iostream>
+
+#endif   // ----- #ifndef main_INC  ----- 
---
+++ sources/abstract_factory_project/implementation_files/main.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "Executing main program." << std::endl;
+    return 0;
+}
