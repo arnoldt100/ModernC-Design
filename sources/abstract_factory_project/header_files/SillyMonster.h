c0889799981ca2d0dd837cf4b981283d84b4437b
arnoldt100 <arnoldt@ornl.gov>
2022-02-25 17:51:39 -0500

    Adding more enemy classes.

---
+++ sources/abstract_factory_project/header_files/SillyMonster.h
@@ -0,0 +1,72 @@
+#ifndef  SillyMonster_INC
+#define  SillyMonster_INC
+
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "Monster.h"
+
+namespace GAME_ENEMIES
+{
+
+     // =====================================================================================
+     //        Class:  SillyMonster
+     //  Description:  
+     //  =====================================================================================
+    class SillyMonster : Monster
+    {
+        public:
+            // ====================  LIFECYCLE     =======================================
+
+            //--------------------------------------------------------------------------------------
+            //       Class:  SillyMonster
+            //      Method:  SillyMonster :: SillyMonster
+            // Description:  
+            // 
+            //  Parameters: 
+            //
+            //      Return:
+            //--------------------------------------------------------------------------------------
+            SillyMonster ();   // constructor
+
+            SillyMonster (const SillyMonster & other);   // copy constructor
+
+            SillyMonster (SillyMonster && other);   // copy-move constructor
+
+            ~SillyMonster ();  // destructor
+
+            // ====================  ACCESSORS     =======================================
+
+            // ====================  MUTATORS      =======================================
+
+            // ====================  OPERATORS     =======================================
+
+            SillyMonster& operator= ( const SillyMonster &other ); // assignment operator
+
+            SillyMonster& operator= ( SillyMonster && other ); // assignment-move operator
+
+        protected:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+        private:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+    }; // -----  end of class SillyMonster  -----
+
+
+}; // namespace GAME_ENEMIES
+
+#endif   /* ----- #ifndef SillyMonster_INC  ----- */
