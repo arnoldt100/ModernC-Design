ad90039a83edae2a2fcd258ccf0c1c3c22fb4b5e
arnoldt100 <arnoldt@ornl.gov>
2022-02-25 17:36:09 -0500

    Adding SillySoldier class.

---
+++ sources/abstract_factory_project/header_files/SillySoldier.h
@@ -0,0 +1,72 @@
+#ifndef  SillySoldier_INC
+#define  SillySoldier_INC
+
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "Soldier.h"
+
+namespace GAME_ENEMIES
+{
+
+     // =====================================================================================
+     //        Class:  SillySoldier
+     //  Description:  
+     //  =====================================================================================
+    class SillySoldier : Soldier
+    {
+        public:
+            // ====================  LIFECYCLE     =======================================
+
+            //--------------------------------------------------------------------------------------
+            //       Class:  SillySoldier
+            //      Method:  SillySoldier :: SillySoldier
+            // Description:  
+            // 
+            //  Parameters: 
+            //
+            //      Return:
+            //--------------------------------------------------------------------------------------
+            SillySoldier ();   // constructor
+
+            SillySoldier (const SillySoldier & other);   // copy constructor
+
+            SillySoldier (SillySoldier && other);   // copy-move constructor
+
+            ~SillySoldier ();  // destructor
+
+            // ====================  ACCESSORS     =======================================
+
+            // ====================  MUTATORS      =======================================
+
+            // ====================  OPERATORS     =======================================
+
+            SillySoldier& operator= ( const SillySoldier &other ); // assignment operator
+
+            SillySoldier& operator= ( SillySoldier && other ); // assignment-move operator
+
+        protected:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+        private:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+    }; // -----  end of class SillySoldier  -----
+
+
+}; // namespace GAME_ENEMIES
+
+#endif   /* ----- #ifndef SillySoldier_INC  ----- */
