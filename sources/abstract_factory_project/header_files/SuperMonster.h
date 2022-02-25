db619641b6f830eafdea93404f30de530df4ca3d
arnoldt100 <arnoldt@ornl.gov>
2022-02-25 17:11:09 -0500

    Added more classes.

---
+++ sources/abstract_factory_project/header_files/SuperMonster.h
@@ -0,0 +1,72 @@
+#ifndef  SuperMonster_INC
+#define  SuperMonster_INC
+
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "enemy.h"
+
+namespace GAME_ENEMIES
+{
+
+     // =====================================================================================
+     //        Class:  SuperMonster
+     //  Description:  
+     //  =====================================================================================
+    class SuperMonster : Enemy
+    {
+        public:
+            // ====================  LIFECYCLE     =======================================
+
+            //--------------------------------------------------------------------------------------
+            //       Class:  SuperMonster
+            //      Method:  SuperMonster :: SuperMonster
+            // Description:  
+            // 
+            //  Parameters: 
+            //
+            //      Return:
+            //--------------------------------------------------------------------------------------
+            SuperMonster ();   // constructor
+
+            SuperMonster (const SuperMonster & other);   // copy constructor
+
+            SuperMonster (SuperMonster && other);   // copy-move constructor
+
+            virtual ~SuperMonster ()=0;  // destructor
+
+            // ====================  ACCESSORS     =======================================
+
+            // ====================  MUTATORS      =======================================
+
+            // ====================  OPERATORS     =======================================
+
+            SuperMonster& operator= ( const SuperMonster &other ); // assignment operator
+
+            SuperMonster& operator= ( SuperMonster && other ); // assignment-move operator
+
+        protected:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+        private:
+            // ====================  METHODS       =======================================
+
+            // ====================  DATA MEMBERS  =======================================
+
+    }; // -----  end of class SuperMonster  -----
+
+
+}; // namespace GAME_ENEMIES
+
+#endif   /* ----- #ifndef SuperMonster_INC  ----- */
